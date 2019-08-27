403bae9c12d839c9edf013a050891b88c6b4231f
Michael Larragueta <m.larragueta@yahoo.com>
2019-08-27 10:23:06 -0700

    Add 3d elements. still buggy

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import heart from './images/heart.mp4'
 import video1 from './images/small/video1.mp4'
 import video2 from './images/small/video2.mp4'
 import logo from './images/thisSVG.svg'
-
+import {CSS3DRenderer, CSS3DObject} from "three/examples/jsm/renderers/CSS3DRenderer";
 // Import and register postprocessing classes as three-native-elements for both react-three-fiber & react-spring
 // They'll be available as native elements <effectComposer /> from then on ...
 import { EffectComposer } from './postprocessing/EffectComposer'
@@ -52,6 +52,67 @@ function App() {
 
 export default App;
 
+function newScene(camera){
+  var scene, renderer;
+  var scene2, renderer2;
+  var controls;
+  init();
+  animate();
+  function init() {
+    // camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
+    // camera.position.set( 200, 200, 0 );
+    //alert("1");
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xf0f0f0 );
+    scene2 = new THREE.Scene();
+    var material = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth: 1, side: THREE.DoubleSide } );
+    //
+    //var element = document.getElementById( 'text2' );
+    for ( var i = 0; i < 1; i ++ ) {
+      var element = document.createElement( 'div' );
+      element.setAttribute("id", "div3d")
+       element.style.zIndex = '-3';
+      element.style.width = '70px';
+      element.style.height = '70px';
+      //element.style.opacity = '0.6';
+      element.style.background = new THREE.Color( Math.random() * 0xffffff ).getStyle();
+      var object = new CSS3DObject( element );
+      object.position.x = Math.random() * 200 - 100;
+      object.position.y = i - 160;
+      object.position.z = 0;
+      object.rotation.x = Math.random();
+      object.rotation.y = Math.random();
+      object.rotation.z = Math.random();
+      object.scale.x = 0.6;
+      object.scale.y = 0.6;
+      scene2.add( object );
+      var geometry = new THREE.PlaneBufferGeometry( 75, 75 );
+      var mesh = new THREE.Mesh( geometry, material );
+      mesh.position.copy( object.position );
+      mesh.rotation.copy( object.rotation );
+      mesh.scale.copy( object.scale );
+      scene.add( mesh );
+    }
+    //
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth - 100, window.innerHeight - 100);
+    document.body.appendChild( renderer.domElement );
+    renderer2 = new CSS3DRenderer();
+    renderer2.setSize( window.innerWidth, window.innerHeight );
+    renderer2.domElement.style.position = 'absolute';
+    renderer2.domElement.style.top = '2vh';
+    document.body.appendChild( renderer2.domElement );
+    //  controls = new TrackballControls( camera, renderer2.domElement );
+  }
+  function animate() {
+    requestAnimationFrame( animate );
+    //controls.update();
+    renderer.render( scene, camera );
+    renderer2.render( scene2, camera );
+  }
+}
+
 function Scene({ top, mouse }) {
   const { size } = useThree()
   const scrollMax = size.height * 4.5  
@@ -72,7 +133,7 @@ function Scene({ top, mouse }) {
       camera.position.y = -((pos - 900) / 100);
     }        
   })
-
+  newScene(camera);
   return (
     <>
       <a.spotLight intensity={1.2} color="white" position={mouse.interpolate((x, y) => [x / 100, -y / 100, 6.5])} />
