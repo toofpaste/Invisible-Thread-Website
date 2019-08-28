d8f9c4868605106aca60052d6d930b40f256be76
Todd Aden <fleuntzap@gmail.com>
2019-08-28 10:37:41 -0700

    add use memo for image loading from data

--- src/sceneElements/Images.jsx
+++ src/sceneElements/Images.jsx
@@ -4,19 +4,27 @@ import { apply as applySpring, useSpring, a, interpolate } from 'react-spring/th
 import data from '../data'
 import { Mesh, Vector3, Material, BufferGeometry, BoxBufferGeometry } from 'three/src/Three';
 
-
-// /** This component creates a bunch of parallaxed images */
+//Image object
 export function Images({ top, mouse, scrollMax }) {
-    // const x = new Mesh();   
-  return data.map(([url, x, y, factor, z, scale], index) => (
+  //Load images from data.js  
+
+  const imageList = useMemo(() => data.map(([url, animation], i) => {    
+    let x = i % 2 == 0 ? 1 : -1;
+
+    let startPosition = [x, -8 - (i * 4), 0]
+    return [url, animation, startPosition];
+  }), [data])
+  
+
+  return imageList.map(([url, animation, [x, y, z]], index) => (
     <Image
       rotation={new THREE.Euler(THREE.Math.degToRad(-90))}
       key={index}
       url={url}
-      scale={scale}
+      scale={1}      
       // opacity={top.interpolate([0, 500], [0, 1])}
       opacity={1}
-      position={new Vector3(0, -10, 0)}
+      position={new Vector3(x, y, z)}
       // position={interpolate([top, mouse], (top, mouse) => [
       //   (-mouse[0] * factor) / 50000 + x,
       //   (mouse[1] * factor) / 50000 + y * 1.15 + ((top * factor) / scrollMax) * 2,
@@ -29,7 +37,8 @@ export function Images({ top, mouse, scrollMax }) {
 /** This component loads an image and projects it onto a plane */
 export function Image({ url, opacity, scale, ...props }) {
   const texture = useMemo(() => new THREE.TextureLoader().load(url), [url])
-  // const texture = useMemo(() => new THREE.MeshBasicMaterial({ color: new THREE.Color('green'), transparent: true }));
+  // const texture = useMemo(() => new THREE.MeshBasicMaterial({ color: new THREE.Color('green'), transparent: true }));  
+  console.log(url);
   
   // let video = document.getElementById('video3');
   // video.play();
